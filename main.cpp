82919f5f79445d771de18e731879adb84919633b
Shadow <94984080+TrueShadow01@users.noreply.github.com>
2026-02-12 14:57:42 +0100

    Add console interactivity via commands (motion, lockdown, fail, reset and exit)

--- main.cpp
+++ main.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
+#include <string>
 #include "ContainmentSystem.h"
 
 int main() {
 	ContainmentSystem system;
 
-	system.handleEvent(ContainmentSystem::Event::MotionDetected);
-	system.handleEvent(ContainmentSystem::Event::ManualLockdown);
-	system.handleEvent(ContainmentSystem::Event::SystemFailure);
-	system.handleEvent(ContainmentSystem::Event::SystemReset);
-	system.handleEvent(ContainmentSystem::Event::SystemFailure);
-	system.handleEvent(ContainmentSystem::Event::ManualLockdown);
-	system.handleEvent(ContainmentSystem::Event::SystemReset);
+	std::string input;
+
+	while (true) {
+		std::cout << "> ";
+		std::cin >> input;
+
+		if (input == "motion") {
+			system.handleEvent(ContainmentSystem::Event::MotionDetected);
+		}
+		if (input == "lockdown") {
+			system.handleEvent(ContainmentSystem::Event::ManualLockdown);
+		}
+		if (input == "fail") {
+			system.handleEvent(ContainmentSystem::Event::SystemFailure);
+		}
+		if (input == "reset") {
+			system.handleEvent(ContainmentSystem::Event::SystemReset);
+		}
+		if (input == "exit") {
+			break;
+		}
+	}
+
 	return 0;
 }
